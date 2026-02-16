76788eca7d8aab438b65380b42745f48d382587d
jwiegner <jwiegner@ethz.ch>
2026-02-16 16:12:34 +0100

    test: simplify plugin discovery test

--- tests/plugins/test_plugin_manager.py
+++ tests/plugins/test_plugin_manager.py
@@ -1,11 +1,10 @@
 from zen_garden.plugin_manager import PluginManager
 
 
-def test_plugin_manager():
-    """Verify PluginManager discovers the test_plugin package."""
+def test_plugin_manager_plugin_discovery():
+    """Verify PluginManager discovers expected plugins."""
     pm = PluginManager()
     discovered = set(pm.discover_plugins())
-    pm.print_plugins()
 
     expected = {"zen_garden.plugins.test_plugin"}
     assert discovered == expected
